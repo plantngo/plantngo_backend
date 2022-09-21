b339fdc53ae25a6d998b164310d4805ae123205e
Gabriel Ong <ojh809@gmail.com>
2022-09-21 08:36:53 +0800

    Updated Customer

--- src/main/java/me/plantngo/backend/models/Customer.java
+++ src/main/java/me/plantngo/backend/models/Customer.java
@@ -6,6 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
 import lombok.*;
 
 
