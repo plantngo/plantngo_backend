da79041ea29b11edc3330abc4ebf49c94bc02826
Gabriel Ong <ojh809@gmail.com>
2022-09-23 16:49:38 +0800

    Added basic Authentication function

--- src/main/java/me/plantngo/backend/security/WebSecurityConfig.java
+++ src/main/java/me/plantngo/backend/security/WebSecurityConfig.java
@@ -1,15 +1,36 @@
 package me.plantngo.backend.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
     
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public WebSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+     
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+ 
+        return authProvider;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -27,5 +48,11 @@ public class WebSecurityConfig {
         .formLogin().disable()
         .headers().disable(); // Disable the security headers, as we do not return HTML in our service
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        // auto-generate a random salt internally
+        return new BCryptPasswordEncoder();
     }
 }
