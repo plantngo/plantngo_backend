e436ddc710d4e49153f2f37a00403b697c2b8e0d
soonann <soonann.dev@gmail.com>
2022-09-27 09:24:18 +0800

    added home controller for ecs healthchecks

---
+++ src/main/java/me/plantngo/backend/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package me.plantngo.backend.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class HomeController {
+    @GetMapping
+    public String homePage(){
+        return "Health Check Endpoint";
+    }
+
+}
