df8ef1279504e3ac60f94ed17710480f4ca01a29
Declan Joshua Tan <declantan.2021@scis.smu.edu.sg>
2022-10-09 17:43:20 +0800

    implemented Customer-Voucher CRUD operations and logic.

---
+++ src/main/java/me/plantngo/backend/DTO/VoucherPurchaseDTO.java
@@ -0,0 +1,19 @@
+package me.plantngo.backend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import me.plantngo.backend.models.Merchant;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+public class VoucherPurchaseDTO {
+
+    @NotNull
+    Integer id;
+
+    @NotNull
+    String merchantName;
+
+}
