6200c2481798271eb80dfe952783ca6da8eccce0
soonann <soonann.dev@gmail.com>
2022-10-15 23:32:12 +0800

    changed healthcheck endpoint to single slash

--- src/main/java/me/plantngo/backend/controllers/HomeController.java
+++ src/main/java/me/plantngo/backend/controllers/HomeController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HomeController {
     @GetMapping
-    public String homePage(){
+    public String homePage() {
         return "Health Check Endpoint";
     }
 
