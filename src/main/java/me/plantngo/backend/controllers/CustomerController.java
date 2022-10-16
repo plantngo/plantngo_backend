5f6a3b75f9e0fd0891323618f08d22463034c5ce
Jing kai Ooi <ooijingkai@gmail.com>
2022-10-16 09:35:03 +0800

    fixed imports

--- src/main/java/me/plantngo/backend/controllers/CustomerController.java
+++ src/main/java/me/plantngo/backend/controllers/CustomerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ import javax.validation.Valid;
 @RequestMapping(path = "api/v1/customer")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CustomerController {
-    
+
     private final CustomerService customerService;
 
     @Autowired
@@ -34,16 +35,17 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping(path="/{username}")
-    //@PreAuthorize("authentication.principal.username == #username || hasRole('ADMIN')")
+    @GetMapping(path = "/{username}")
+    // @PreAuthorize("authentication.principal.username == #username ||
+    // hasRole('ADMIN')")
     public Customer getUserByUsername(@PathVariable("username") String username) {
         return customerService.getCustomerByUsername(username);
     }
 
-    @PostMapping(path="/{username}")
-    public void addGreenPoints(@PathVariable("username") String username, @RequestBody Integer amount){
+    @PostMapping(path = "/{username}")
+    public void addGreenPoints(@PathVariable("username") String username, @RequestBody Integer amount) {
         customerService.addGreenPoints(username, amount);
-//        return username + " amount:" + amount.toString();
+        // return username + " amount:" + amount.toString();
     }
 
     @GetMapping
@@ -51,16 +53,16 @@ public class CustomerController {
         return customerService.findAll();
     }
 
-    @PutMapping(path="/{username}")
-    public ResponseEntity<Customer> updateCustomer(@PathVariable("username") String username, @RequestBody UpdateCustomerDTO updateCustomerDTO) {
+    @PutMapping(path = "/{username}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("username") String username,
+            @RequestBody UpdateCustomerDTO updateCustomerDTO) {
         Customer customer = customerService.updateCustomer(username, updateCustomerDTO);
         return new ResponseEntity<>(customer, HttpStatus.OK);
     }
 
-    @DeleteMapping(path="/{username}")
+    @DeleteMapping(path = "/{username}")
     public ResponseEntity<String> deleteCustomer(@PathVariable("username") String username) {
         customerService.deleteCustomer(username);
         return new ResponseEntity<>("Customer deleted!", HttpStatus.OK);
     }
 }
-
