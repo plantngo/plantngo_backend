29e2a81bcb06fb7a58093e68206d19ee7bf3d6bf
Gabriel Ong <ojh809@gmail.com>
2022-10-18 12:33:37 +0800

    Bug Fixes

--- src/main/java/me/plantngo/backend/models/Product.java
+++ src/main/java/me/plantngo/backend/models/Product.java
@@ -20,6 +20,7 @@ import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import lombok.*;
