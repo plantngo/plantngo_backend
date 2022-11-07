ee30139ba01627cd1937314a6435c9d3642a4ae7
Gabriel Ong <ojh809@gmail.com>
2022-11-07 13:12:45 +0800

    Added some unit tests for ProductService

--- src/test/java/me/plantngo/backend/services/ProductServiceTest.java
+++ src/test/java/me/plantngo/backend/services/ProductServiceTest.java
@@ -1,6 +1,16 @@
 package me.plantngo.backend.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -35,7 +45,58 @@ public class ProductServiceTest {
     private Ingredient ingredient;
 
     @BeforeEach
-    public void initEach() {
+    void initEach() {
 
+        ingredient = new Ingredient(null, null, "Beef", null, 10.0, null);
+        Ingredient ingredient2 = new Ingredient(null, null, "Coffee", null, 20.0, null);
+
+        product = new Product(null, "Steak", 10.0, null, 10.0, null, null, null, null, null);
+        productIngredient = new ProductIngredient(null, null, ingredient2, product);
+
+        product.setProductIngredients(Set.of(productIngredient));
+
+    }
+
+    @Test
+    void testGetAllProducts_AllOrders_ReturnAllOrders() {
+        
+        // Arrange
+        Product product2 = new Product();
+        product2.setName("Sprite");
+        Product product3 = new Product();
+        product3.setName("Broccoli");
+        Product product4 = new Product();
+        product4.setName("Lettuce");
+        List<Product> expectedProducts = new ArrayList<>();
+        expectedProducts.add(product);
+        expectedProducts.add(product2);
+        expectedProducts.add(product3);
+        expectedProducts.add(product4);
+
+        when(productRepository.findAll())
+            .thenReturn(expectedProducts);
+
+        // Act
+        List<Product> responseProducts = productService.getAllProducts();
+
+        // Assert
+        assertEquals(expectedProducts, responseProducts);
+        verify(productRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testGetAllProducts_NoOrders_ReturnEmptyList() {
+
+        // Arrange
+        List<Product> expectedProducts = new ArrayList<>();
+        when(productRepository.findAll())
+            .thenReturn(expectedProducts);
+
+        // Act
+        List<Product> responseProducts = productService.getAllProducts();
+
+        // Assert
+        assertEquals(expectedProducts, responseProducts);
+        verify(productRepository, times(1)).findAll();
     }
 }
