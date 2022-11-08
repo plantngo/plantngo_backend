80be9c3299e05c896d08ff308cb615058cc1d694
Gabriel Ong <ojh809@gmail.com>
2022-11-08 15:44:15 +0800

    Edited ChangeCredentialServiceTest

--- src/test/java/me/plantngo/backend/services/ChangeCredentialServiceTest.java
+++ src/test/java/me/plantngo/backend/services/ChangeCredentialServiceTest.java
@@ -2,6 +2,7 @@ package me.plantngo.backend.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
